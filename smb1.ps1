16f84a234d97ab6671ca41bc61be086d30e7136a
gdanldo <24873103+gdanldo@users.noreply.github.com>
2020-06-12 22:32:25 +0200

    Add files via upload

---
+++ smb1.ps1
@@ -0,0 +1,3 @@
+﻿#https://docs.microsoft.com/en-us/windows-server/storage/file-server/troubleshoot/detect-enable-and-disable-smbv1-v2-v3
+Get-WindowsFeature FS-SMB1
+Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
